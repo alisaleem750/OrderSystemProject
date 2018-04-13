424fb046796bc9eaa87decb66c40fd71aba17a03
alisaleem750 <alisaleem750@gmail.com>
2018-04-13 17:11:17 +0100

    Friday evening

--- src/OrderClient/NewOrderSingle.java
+++ src/OrderClient/NewOrderSingle.java
@@ -8,9 +8,12 @@ public class NewOrderSingle implements Serializable{
 	public int size;
 	public float price;
 	public Instrument instrument;
+	public char OrdStatus;
+
 	public NewOrderSingle(int size,float price,Instrument instrument){
 		this.size=size;
 		this.price=price;
 		this.instrument=instrument;
+		this.OrdStatus='A';
 	}
 }
